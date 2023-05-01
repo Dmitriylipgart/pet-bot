23aa5acd7d56c1709e39f3b1d5ef4e602b2e34db
DLipgart <dmitriy.lipgart@kandasoft.com>
2023-05-01 18:35:22 +0300

    added docker and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { BotModule } from './bot/bot.module';
+import * as LocalSession from 'telegraf-session-local';
+
+const sessions = new LocalSession({ database: 'session_db.json' });
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: ormConfig,
+    }),
+    AutomapperModule.forRoot({
+      strategyInitializer: classes(),
+    }),
+    TelegrafModule.forRoot({
+      middlewares: [sessions.middleware()],
+      token: process.env.BOT_TOKEN,
+    }),
+    BotModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
